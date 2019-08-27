c228695058419dbeb31fc337970f3877006eef7e
Jorge Corradi <jlcorradi@gmail.com>
2019-08-27 11:37:07 -0300

    More tweaks

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import SpaTemplate from './SpaTemplate';
+
+import SpaTemplate from '../components/SpaTemplate';
 import HomeView from '../views/HomeView';
 import AboutView from '../views/AboutView';
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "font-awesome/css/font-awesome.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
-import "bootstrap";
+import 'bootstrap';
 
-import "./styles/main.scss";
-import "./assets/themify/themify-icons.css";
-import App from "./components/App";
+import './styles/main.scss';
+import './assets/themify/themify-icons.css';
+import App from './App';
 
 ReactDOM.render(
   <Router>
     <App></App>
   </Router>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
